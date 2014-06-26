a337b89c74d0c945564bda6a8965b05e0c2fb63d
Mihnea Simian <contact@mesimian.com>
2014-06-26 08:13:30 +0300

    Internationalization and order flow sample settings

--- oscaro/oscaro/settings.py
+++ oscaro/oscaro/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ro_RO'
 
 TIME_ZONE = 'UTC'
 
--- oscaro/oscaro/urls.py
+++ oscaro/oscaro/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'', include(application.urls)),
 )
